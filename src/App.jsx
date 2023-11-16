273290b3d89061b4130cf5e49784848cee5ade6c
Your Name <you@example.com>
2023-11-16 22:06:44 +0100

    gioco

--- src/App.jsx
+++ src/App.jsx
@@ -1,6 +1,7 @@
 import styles from './App.module.scss';
 import { BrowserRouter,Routes, Route } from "react-router-dom";
 
+import Layouts from './layouts/Layouts';
 import Home from './pages/Home';
 import CampoMinato from "./pages/CampoMinato/CampoMinato";
 import Tetris from "./pages/Tetris";
@@ -11,9 +12,11 @@ function App() {
       <div className={styles.App}>
 <BrowserRouter>     
       <Routes>
+      <Route element={<Layouts />}>
       <Route path="/" element={<Home/>}/>
       <Route path="/campominato" element={<CampoMinato/>}/>
       <Route path="/Tetris" element={<Tetris/>}/>
+      </Route>
       </Routes>
      </BrowserRouter>
      
--- src/layouts/Layouts.jsx
+++ src/layouts/Layouts.jsx
@@ -1,15 +1,16 @@
 import { Outlet } from "react-router-dom";
 import styles from "./index.module.scss";
-import Home from "../pages/Home";
-import CampoMinato from "../pages/CampoMinato";
-import Tetris from "../pages/Tetris";
+// import Home from "../pages/Home";
+
+// import Tetris from "../pages/Tetris";
 const Layouts =()=>{
     return (
         <div className={styles.Layouts}>
-            <Home/>
-<CampoMinato/>
-<Outlet/>
-            <Tetris/>
+          
+
+        <Outlet />
+     
+           
         </div>
     )
 }
